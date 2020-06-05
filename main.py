d1972cbb95a74834b0a72751c884b6e3bb5f2ed0
Arsen <ar.poryadin@yandex.ru>
2020-06-05 11:42:24 +0800

    without proper cridentials obtaining

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import sys
+import requests
 import boto3
+
+
+def obtain_temporary_credentials() -> (str, str, str):
+    """Obtain temporary credentials from AWS IoT device certificate and private key."""
+    credential_provider_endpoint = 'https://c2svzaf1dnajce.credentials.iot.us-east-1.amazonaws.com/role-aliases/iot-s3-access-role-alias/credentials'
+    device_cert_path = '~/certs/aws_iot_arsen/certificate.pem.crt'
+    device_private_key_path = '~/certs/aws_iot_arsen/private.pem.key'
+    resp = requests.get(
+        credential_provider_endpoint,
+        headers={'x-amzn-iot-thingname': 'RaspberryPi'},
+        cert=(device_cert_path, device_private_key_path),
+    )
+
+    if resp:  # check whether https request succeeds
+        credentials = resp.json()
+        access_key_id = credentials['credentials']['accessKeyId']
+        secrete_access_key = credentials['credentials']['secretAccessKey']
+        session_token = credentials['credentials']['sessionToken']
+        return access_key_id, secrete_access_key, session_token
+    else:
+        print('error requesting temporary access to AWS S3')
+        return '', '', ''
+
 
 s3_client = boto3.client('s3')
 
 
 def upload_to_s3(local_file, bucket, s3_file):
+
+
     try:
         s3_client.upload_file(local_file, bucket, s3_file)
         print("Upload Successful")
